007627c17637857789e03ad987ff861af9731ae3
hitfangsong <sof10@pitt.edu>
2019-09-21 17:53:40 -0400

    Signed-off-by: hitfangsong <sof10@pitt.edu>

---
+++ spring-boot-quick-start/src/main/java/com/springboot/springbootquickstart/bean/Person.java
@@ -0,0 +1,89 @@
+package com.springboot.springbootquickstart.bean;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+@Component
+@ConfigurationProperties(prefix = "person")
+public class Person {
+    private String name;
+    private Integer age;
+    private Boolean boss;
+    private Date birth;
+    private Map<String,Object> maps;
+    private List<Object> lists;
+    private Dog dog;
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", boss=" + boss +
+                ", birth=" + birth +
+                ", maps=" + maps +
+                ", lists=" + lists +
+                ", dog=" + dog +
+                '}';
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public void setBoss(Boolean boss) {
+        this.boss = boss;
+    }
+
+    public void setBirth(Date birth) {
+        this.birth = birth;
+    }
+
+    public void setMaps(Map<String, Object> maps) {
+        this.maps = maps;
+    }
+
+    public void setLists(List<Object> lists) {
+        this.lists = lists;
+    }
+
+    public void setDog(Dog dog) {
+        this.dog = dog;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public Boolean getBoss() {
+        return boss;
+    }
+
+    public Date getBirth() {
+        return birth;
+    }
+
+    public Map<String, Object> getMaps() {
+        return maps;
+    }
+
+    public List<Object> getLists() {
+        return lists;
+    }
+
+    public Dog getDog() {
+        return dog;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
--- spring-boot-quick-start/src/test/java/com/springboot/springbootquickstart/SpringBootQuickStartApplicationTests.java
+++ spring-boot-quick-start/src/test/java/com/springboot/springbootquickstart/SpringBootQuickStartApplicationTests.java
@@ -1,16 +1,25 @@
 package com.springboot.springbootquickstart;
 
+import com.springboot.springbootquickstart.bean.Person;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+/*
+Spring boot's Junit
+ */
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringBootQuickStartApplicationTests {
 
+    @Autowired
+    Person person;
+
     @Test
     public void contextLoads() {
+        System.out.println(person);
     }
 
 }
