d171cee5391ddf49e5ab9eacc854ef90a17e1e76
hitfangsong <sof10@pitt.edu>
2019-09-21 16:43:45 -0400

    Signed-off-by: hitfangsong <sof10@pitt.edu>

--- springboot01/src/main/java/com/springDemo/HelloWorldMainApplication.java
+++ springboot01/src/main/java/com/springDemo/HelloWorldMainApplication.java
@@ -1,4 +1,11 @@
 package com.springDemo;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class HelloWorldMainApplication {
+    public static void main(String [] args){
+        SpringApplication.run(HelloWorldMainApplication.class,args);
+    }
 }
